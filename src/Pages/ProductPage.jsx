b235f4e2dfaea809741f96465ae0f4821a17b895
FatemeArefi <f.arefi982@gmail.com>
2024-10-07 22:07:58 +0330

    list of Products rendering

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,9 +1,19 @@
 import { useProducts } from "../context/ProductContext";
-
+import styles from "./ProductsPage.module.css";
 function ProductPage() {
   const products = useProducts();
   console.log(products);
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p>}
+        {products.map((p) => (
+          <p key={p.id}>{p.title}</p>
+        ))}
+      </div>
+      <div>Sidebar</div>
+    </div>
+  );
 }
 
 export default ProductPage;
