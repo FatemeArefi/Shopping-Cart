dea5e2ffbf0626b22bcd7f8d87e3c2058af95237
FatemeArefi <f.arefi982@gmail.com>
2024-10-07 23:42:16 +0330

    search button added

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import Cart from "../components/Cart";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
+
 import styles from "./ProductsPage.module.css";
+
 function ProductPage() {
   const products = useProducts();
+
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {
+    console.log("Search");
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Cart key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>Sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Cart key={p.id} data={p} />
+          ))}
+        </div>
+        <div>Sidebar</div>
+      </div>
+    </>
   );
 }
 
