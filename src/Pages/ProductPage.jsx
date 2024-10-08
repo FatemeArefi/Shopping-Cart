87eff8e76db3c61d13563d5290269b6379550388
FatemeArefi <f.arefi982@gmail.com>
2024-10-08 21:15:53 +0330

    string query

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import { ImSearch } from "react-icons/im";
 import { FaListUl } from "react-icons/fa";
+import { useSearchParams } from "react-router-dom";
+
 import Cart from "../components/Cart";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
-import { filterProducts, searchProducts } from "../helpers/helper";
+import {
+  createQueryObject,
+  filterProducts,
+  searchProducts,
+} from "../helpers/helper";
 
 import styles from "./ProductsPage.module.css";
 
@@ -15,26 +21,29 @@ function ProductPage() {
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState({});
 
+  const [serachParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
     setDisplayed(products);
   }, [products]);
 
   useEffect(() => {
+    setSearchParams(query);
+
     let finalProducts = searchProducts(products, query.search);
     finalProducts = filterProducts(finalProducts, query.category);
-
     setDisplayed(finalProducts);
   }, [query]);
 
   const searchHandler = () => {
-    setQuery((query) => ({ ...query, search }));
+    setQuery((query) => createQueryObject(query, { search: search }));
   };
   const categoryHandler = (event) => {
     const { tagName } = event.target;
     const category = event.target.innerText.toLowerCase();
 
     if (tagName !== "LI") return;
-    setQuery((query) => ({ ...query, category }));
+    setQuery((query) => createQueryObject(query, { category }));
   };
 
   return (
--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -14,4 +14,19 @@ const filterProducts = (products, category) => {
   return filterProducts;
 };
 
-export { shortenText, searchProducts, filterProducts };
+const createQueryObject = (currentQuery, newQuery) => {
+  if (newQuery.category === "all") {
+    const { category, ...rest } = currentQuery;
+    return rest;
+  }
+  if (newQuery.search === "") {
+    const { search, ...rest } = currentQuery;
+    return rest;
+  }
+  return {
+    ...currentQuery,
+    ...newQuery,
+  };
+};
+
+export { shortenText, searchProducts, filterProducts, createQueryObject };
