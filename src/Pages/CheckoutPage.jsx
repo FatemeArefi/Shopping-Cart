9116fde3d22f77a4d7ca66b13b7ab4d22065e54d
FatemeArefi <f.arefi982@gmail.com>
2024-10-12 16:35:16 +0330

    sidebar of shopping card

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,5 +1,31 @@
+import BasketCart from "../components/BasketCart";
+import BasketSidbar from "../components/BasketSidbar";
+import { useCart } from "../context/CartContext";
+import styles from "./chechoutPage.module.css";
+
 function CheckoutPage() {
-  return <div>hmm</div>;
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <BasketSidbar state={state} clickHandler={clickHandler} />
+      <div className={styles.product}>
+        {state.selectedItems.map((p) => (
+          <BasketCart key={p.id} data={p} clickHandler={clickHandler} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutPage;
